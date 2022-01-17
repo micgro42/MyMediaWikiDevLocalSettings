4caf02a131fe7f97688d8e7bd7fe61b4b3e3dff4
Michael Große <michael.grosse@wikimedia.de>
2022-01-17 19:56:45 +0100

    Extend createWikis.sh script to create full link between client and repo
    Though some interwiki config might still be wrong

--- createWikis.sh
+++ createWikis.sh
@@ -8,11 +8,14 @@ mw docker phpmyadmin create
 
 # install repo
 mw docker mediawiki install --dbtype=mysql --dbname=default
-# tell the repo that it is also a client to itself
+# tell the repo about itself
 mw docker mediawiki exec -- php maintenance/addSite.php default default --interwiki-id default --navigation-id default --pagepath 'http://default.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://default.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://default.mediawiki.mwdd.localhost:8080'
 
 # install client
 mw docker mediawiki install --dbtype=mysql --dbname=client
+# tell the client about itself
+mw docker mediawiki exec -- php maintenance/addSite.php --wiki client client default --interwiki-id client --navigation-id client --pagepath 'http://client.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://client.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://client.mediawiki.mwdd.localhost:8080'
 # tell the repo about the client wiki
 mw docker mediawiki exec -- php maintenance/addSite.php client default --interwiki-id client --navigation-id client --pagepath 'http://client.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://client.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://client.mediawiki.mwdd.localhost:8080'
-
+# tell the client about the repo wiki
+mw docker mediawiki exec -- php maintenance/addSite.php --wiki client default default --interwiki-id default --navigation-id default --pagepath 'http://default.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://default.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://default.mediawiki.mwdd.localhost:8080'
