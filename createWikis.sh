5981d00513111fe97d5e2266e2135527c25418a7
Michael Große <mic.grosse@googlemail.com>
2022-07-25 22:33:57 +0200

    Localize local sitelink group
    This adds ability to make logged-in edits and add pages.

--- createWikis.sh
+++ createWikis.sh
@@ -27,9 +27,46 @@ mw docker mediawiki exec -- php maintenance/sql.php --wiki client --status --que
 # create "Data Bridge" tag
 mw docker mediawiki exec -- php maintenance/addChangeTag.php --tag 'Data Bridge' --reason 'added by createWikis.sh'
 
+# create bot passwords to use with API below
+mw docker mediawiki exec -- php maintenance/createBotPassword.php --appid 'createWikis.sh' --grants 'basic,createeditmovepage,editinterface,editpage' 'Admin' '00000000000000000000000000000000'
+
 ####
-# Create starting entities
+# Create starting entities and pages
 ###
+token=""
+apiBase='http://default.mediawiki.mwdd.localhost:8080/w/api.php'
+curlCookieOptions="-b '/tmp/cookie.txt' -c '/tmp/cookie.txt'"
+
+getCSRFToken () {
+  local user='Admin@createWikis.sh'
+  local pass='00000000000000000000000000000000'
+
+  local loginTokenResponse=`curl -j ${curlCookieOptions} "${apiBase}?action=query&meta=tokens&type=login&format=json&formatversion=2"`
+
+  local loginToken=`grep --only-matching -E '[a-f0-9]{40}' <<< $loginTokenResponse`
+
+  curl ${curlCookieOptions} \
+    --data-urlencode "lgtoken=${loginToken}+\\" \
+    --data-urlencode "lgname=${user}" \
+    --data-urlencode "lgpassword=${pass}" \
+    "${apiBase}?action=login&format=json&formatversion=2"
+
+  local tokenResponse=`curl -b '/tmp/cookie.txt' -c '/tmp/cookie.txt' "${apiBase}?action=query&meta=tokens&format=json&formatversion=2"`
+  token=`grep --only-matching -E '[a-f0-9]{40}' <<< $tokenResponse`
+}
+getCSRFToken
+
+createPage () {
+  local title=$1
+  local text=$2
+  local summary='Created by createWikis.sh'
+  curl ${curlCookieOptions} --data-urlencode  "token=${token}+\\" \
+  --data-urlencode "title=${title}" \
+  --data-urlencode "text=${text}" \
+  --data-urlencode "summary=${summary}" \
+  "${apiBase}?action=edit&bot=1&format=json&formatversion=2"
+}
+
 createEntity () {
   local entityType=$1
   local data=$2
@@ -42,3 +79,6 @@ createEntity () {
 
 # Q1 - Item for "Good Article" badge
 createEntity 'item' '{"labels":{"en":{"language":"en","value":"Good Article"}}}'
+
+# localize sitelinks group
+createPage 'MediaWiki:Wikibase-sitelinks-mylocalwikis' 'Local Wikis'
