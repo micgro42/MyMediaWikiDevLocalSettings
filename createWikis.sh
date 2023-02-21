df19ee4742ebf6d841bc6843708fa2614fd21c92
Michael Große <michael.grosse@wikimedia.de>
2023-02-21 17:52:41 +0100

    Run all maint scripts with run.php
    That is the new way in 1.40, also it would print annoying messages
    otherwise.

--- createWikis.sh
+++ createWikis.sh
@@ -13,26 +13,26 @@ mw docker memcached create
 # install repo
 mw docker mediawiki install --dbtype=mysql --dbname=wikidatawiki_dev
 # tell the repo about itself
-mw docker mediawiki exec -- php maintenance/addSite.php --wiki wikidatawiki_dev wikidatawiki_dev mylocalwikis --interwiki-id wikidatawiki_dev --navigation-id wikidatawiki_dev --pagepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080'
+mw docker mediawiki exec -- php maintenance/run.php AddSite --wiki wikidatawiki_dev wikidatawiki_dev mylocalwikis --interwiki-id wikidatawiki_dev --navigation-id wikidatawiki_dev --pagepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080'
 
 # install dewiki_dev
 mw docker mediawiki install --dbtype=mysql --dbname=dewiki_dev
 # tell the dewiki_dev about itself
-mw docker mediawiki exec -- php maintenance/addSite.php --wiki dewiki_dev dewiki_dev mylocalwikis --interwiki-id dewiki_dev --navigation-id dewiki_dev --pagepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://dewiki_dev.mediawiki.mwdd.localhost:8080'
+mw docker mediawiki exec -- php maintenance/run.php AddSite --wiki dewiki_dev dewiki_dev mylocalwikis --interwiki-id dewiki_dev --navigation-id dewiki_dev --pagepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://dewiki_dev.mediawiki.mwdd.localhost:8080'
 # tell the repo about the dewiki_dev wiki
-mw docker mediawiki exec -- php maintenance/addSite.php --wiki wikidatawiki_dev dewiki_dev mylocalwikis --interwiki-id dewiki_dev --navigation-id dewiki_dev --pagepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://dewiki_dev.mediawiki.mwdd.localhost:8080'
+mw docker mediawiki exec -- php maintenance/run.php AddSite --wiki wikidatawiki_dev dewiki_dev mylocalwikis --interwiki-id dewiki_dev --navigation-id dewiki_dev --pagepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language de --server 'http://dewiki_dev.mediawiki.mwdd.localhost:8080'
 # tell the dewiki_dev about the repo wiki
-mw docker mediawiki exec -- php maintenance/addSite.php --wiki dewiki_dev wikidatawiki_dev mylocalwikis --interwiki-id wikidatawiki_dev --navigation-id wikidatawiki_dev --pagepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080'
+mw docker mediawiki exec -- php maintenance/run.php AddSite --wiki dewiki_dev wikidatawiki_dev mylocalwikis --interwiki-id wikidatawiki_dev --navigation-id wikidatawiki_dev --pagepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=$1' --filepath 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/$1' --language en --server 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080'
 
 # add interwiki prefixes so that the sitelinks in the sidebar work
-mw docker mediawiki exec -- php maintenance/sql.php --wiki wikidatawiki_dev --status --query "INSERT INTO interwiki (iw_prefix, iw_url, iw_local, iw_trans, iw_api, iw_wikiid) VALUES ('dewiki_dev', 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=\$1', 0, 0, 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/api.php', 'dewiki_dev');"
-mw docker mediawiki exec -- php maintenance/sql.php --wiki dewiki_dev --status --query "INSERT INTO interwiki (iw_prefix, iw_url, iw_local, iw_trans, iw_api, iw_wikiid) VALUES ('wikidatawiki_dev', 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=\$1', 0, 0, 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/api.php', 'wikidatawiki_dev');"
+mw docker mediawiki exec -- php maintenance/run.php MwSql --wiki wikidatawiki_dev --status --query "INSERT INTO interwiki (iw_prefix, iw_url, iw_local, iw_trans, iw_api, iw_wikiid) VALUES ('dewiki_dev', 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=\$1', 0, 0, 'http://dewiki_dev.mediawiki.mwdd.localhost:8080/w/api.php', 'dewiki_dev');"
+mw docker mediawiki exec -- php maintenance/run.php MwSql --wiki dewiki_dev --status --query "INSERT INTO interwiki (iw_prefix, iw_url, iw_local, iw_trans, iw_api, iw_wikiid) VALUES ('wikidatawiki_dev', 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/index.php?title=\$1', 0, 0, 'http://wikidatawiki_dev.mediawiki.mwdd.localhost:8080/w/api.php', 'wikidatawiki_dev');"
 
 # create "Data Bridge" tag
-mw docker mediawiki exec -- php maintenance/addChangeTag.php --wiki wikidatawiki_dev --tag 'Data Bridge' --reason 'added by createWikis.sh'
+mw docker mediawiki exec -- php maintenance/run.php AddChangeTag --wiki wikidatawiki_dev --tag 'Data Bridge' --reason 'added by createWikis.sh'
 
 # create bot passwords to use with API below
-mw docker mediawiki exec -- php maintenance/createBotPassword.php --wiki wikidatawiki_dev --appid 'createWikis.sh' --grants 'basic,createeditmovepage,editinterface,editpage' 'Admin' '00000000000000000000000000000000'
+mw docker mediawiki exec -- php maintenance/run.php CreateBotPassword --wiki wikidatawiki_dev --appid 'createWikis.sh' --grants 'basic,createeditmovepage,editinterface,editpage' 'Admin' '00000000000000000000000000000000'
 
 ####
 # Create starting entities and pages
